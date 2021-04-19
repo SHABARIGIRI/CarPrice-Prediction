d3126d91e41609a396603f6ab006ebba91e13f75
SHABARI GIRISH <55288790+SHABARIGIRI@users.noreply.github.com>
2021-04-19 14:40:53 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,54 +1,67 @@
-from flask import Flask, render_template, request
-import jsonify
-import requests
+from pywebio.platform.flask import webio_view
+from pywebio import STATIC_PATH
+from flask import Flask, send_from_directory
+from pywebio.input import *
+from pywebio.output import *
+import argparse
+from pywebio import start_server
+
 import pickle
 import numpy as np
-import sklearn
-from sklearn.preprocessing import StandardScaler
+model = pickle.load(open('regression_rf.pkl', 'rb'))
 app = Flask(__name__)
-model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
-@app.route('/',methods=['GET'])
-def Home():
-    return render_template('index.html')
 
 
-standard_to = StandardScaler()
-@app.route("/predict", methods=['POST'])
 def predict():
-    Fuel_Type_Diesel=0
-    if request.method == 'POST':
-        Year = int(request.form['Year'])
-        Present_Price=float(request.form['Present_Price'])
-        Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
-        Owner=int(request.form['Owner'])
-        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
-        if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
-        else:
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
-        Year=2020-Year
-        Seller_Type_Individual=request.form['Seller_Type_Individual']
-        if(Seller_Type_Individual=='Individual'):
-            Seller_Type_Individual=1
-        else:
-            Seller_Type_Individual=0	
-        Transmission_Mannual=request.form['Transmission_Mannual']
-        if(Transmission_Mannual=='Mannual'):
-            Transmission_Mannual=1
-        else:
-            Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
-        output=round(prediction[0],2)
-        if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
-        else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+    Year = input("Enter the Model Year：", type=NUMBER)
+    Year = 2021 - Year
+    Present_Price = input("Enter the Present Price(in LAKHS)", type=FLOAT)
+    Kms_Driven = input("Enter the distance it has travelled(in KMS)：", type=FLOAT)
+    Kms_Driven2 = np.log(Kms_Driven)
+    Owner = input("Enter the number of owners who have previously owned it(0 or 1 or 2 or 3)", type=NUMBER)
+    Fuel_Type = select('What is the Fuel Type', ['Petrol', 'Diesel','CNG'])
+    if (Fuel_Type == 'Petrol'):
+        Fuel_Type = 239
+
+    elif (Fuel_Type == 'Diesel'):
+        Fuel_Type = 60
+
     else:
-        return render_template('index.html')
+        Fuel_Type = 2
+    Seller_Type = select('Are you a dealer or an individual', ['Dealer', 'Individual'])
+    if (Seller_Type == 'Individual'):
+        Seller_Type = 106
 
-if __name__=="__main__":
-    app.run(debug=True)
+    else:
+        Seller_Type = 195
+    Transmission = select('Transmission Type', ['Manual Car', 'Automatic Car'])
+    if (Transmission == 'Manual Car'):
+        Transmission = 261
+    else:
+        Transmission = 40
 
+    prediction = model.predict([[Present_Price, Kms_Driven2, Fuel_Type, Seller_Type, Transmission, Owner, Year]])
+    output = round(prediction[0], 2)
+
+    if output < 0:
+        put_text("Sorry You can't sell this Car")
+
+    else:
+        put_text('You can sell this Car at price:',output)
+
+app.add_url_rule('/tool', 'webio_view', webio_view(predict),
+            methods=['GET', 'POST', 'OPTIONS'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    start_server(predict, port=args.port)
+#if __name__ == '__main__':
+    #predict()
+
+#app.run(host='localhost', port=80)
+
+#visit http://localhost/tool to open the PyWebIO application.
